c32974e942922b33f296307baa23180768f0c9e0
rogtang <rogtang@gmail.com>
2021-12-27 13:07:24 -0800

    new server config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: "https://imbibe-server.herokuapp.com/api",
-  //API_ENDPOINT: 'http://localhost:8000/api',
+  //API_ENDPOINT: "https://imbibe-server.herokuapp.com/api",
+  API_ENDPOINT: "https://frozen-dawn-91192.herokuapp.com/api",
+//API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: "imbibe-client-auth-token",
 };
