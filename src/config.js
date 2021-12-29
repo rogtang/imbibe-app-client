f09ca5dae61603a93b26ad11258ccc78e2c3d42c
rogtang <rogtang@gmail.com>
2021-12-28 18:09:51 -0800

    refactor api

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
   //API_ENDPOINT: "https://imbibe-server.herokuapp.com/api",
-  API_ENDPOINT: "https://frozen-dawn-91192.herokuapp.com/api",
+  API_ENDPOINT: window.location.hostname === "localhost"? 'http://localhost:8000/api' :'https://frozen-dawn-91192.herokuapp.com/api',
+  //API_ENDPOINT: 'https://frozen-dawn-91192.herokuapp.com/api',
 //API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: "imbibe-client-auth-token",
 };
